bcbac467e743542c54a8f213a53ceca2f315627c
Lucas <Lucas.amadio@atmsistema.com.br>
2025-01-21 12:01:21 -0300

    inicio header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} antialiased`}
       >
-        <Header />
+        {/* <Header /> */}
 
         {children}
         <Footer />
