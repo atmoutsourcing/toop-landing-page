02895699367c02839f4f87c1246b10d90c4bda18
Pedro Oliveira <pedrocanuto2002@gmail.com>
2025-01-27 11:39:47 -0300

    ajustando responsividade

--- src/app/aboutUs/page.tsx
+++ src/app/aboutUs/page.tsx
@@ -22,7 +22,7 @@ export default function AboutUs() {
 
   return (
     <div className="flex flex-col font-inter">
-      <div className="group relative flex min-h-[600px] cursor-pointer flex-col items-center justify-center overflow-hidden px-56 group-hover:duration-1000">
+      <div className="group relative flex min-h-[600px] cursor-pointer flex-col items-center justify-center overflow-hidden group-hover:duration-1000 md:px-56">
         <h1
           style={{ zIndex: '100' }}
           className="text-center text-5xl font-bold dark:text-zinc-200"
@@ -32,11 +32,14 @@ export default function AboutUs() {
         <h1 className="text-center text-5xl font-bold">
           <span className="text-yellow-400">transformar</span> sua gestão!
         </h1>
-        <div className="before:absolute before:left-[25%] before:top-[40%] before:h-[204px] before:w-[213px] before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-blue-500 before:blur-[120px] before:duration-1000 group-hover:before:translate-x-[350%]" />
-        <div className="before:absolute before:left-[68%] before:top-[55%] before:h-[168px] before:w-[151px] before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-blue-600 before:blur-[120px] before:duration-1000 group-hover:before:translate-x-[-500%]" />
+        <div className="absolute left-[0%] top-[20%] flex h-[140px] w-[213px] bg-blue-500 blur-[100px] before:rounded-full md:hidden" />
+        <div className="absolute left-[60%] top-[55%] flex h-[140px] w-[100px] bg-blue-500 blur-[80px] before:rounded-full md:hidden" />
+
+        <div className="hidden before:absolute before:left-[25%] before:top-[40%] before:h-[204px] before:w-[213px] before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-blue-500 before:blur-[120px] before:duration-1000 group-hover:before:translate-x-[350%] md:flex" />
+        <div className="hidden before:absolute before:left-[68%] before:top-[55%] before:h-[168px] before:w-[151px] before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-blue-600 before:blur-[120px] before:duration-1000 group-hover:before:translate-x-[-500%] md:flex" />
       </div>
 
-      <div className="flex h-[205px] w-full flex-col items-center">
+      <div className="flex h-[205px] w-full flex-col items-center pl-10 md:p-0">
         <Carousel
           opts={{ loop: true }}
           plugins={[plugin.current]}
@@ -45,42 +48,54 @@ export default function AboutUs() {
           <CarouselContent>
             <CarouselItem className="pt-10 md:basis-1/2 lg:basis-1/4">
               <div className="flex h-[122px] w-[248px] flex-col items-center justify-center">
-                <h1 className="text-[40px] font-bold text-blue-800">+72.544</h1>
+                <h1 className="text-[40px] font-bold text-blue-500 dark:text-blue-800">
+                  +72.544
+                </h1>
                 <p className="text-[16px]">Smarthphones gerenciados</p>
               </div>
             </CarouselItem>
 
             <CarouselItem className="pt-10 md:basis-1/2 lg:basis-1/4">
               <div className="flex h-[122px] w-[248px] flex-col items-center justify-center">
-                <h1 className="text-[40px] font-bold text-blue-800">+38.025</h1>
+                <h1 className="text-[40px] font-bold text-blue-500 dark:text-blue-800">
+                  +38.025
+                </h1>
                 <p className="text-[16px]">Manutenções feitas</p>
               </div>
             </CarouselItem>
 
             <CarouselItem className="pt-10 md:basis-1/2 lg:basis-1/4">
               <div className="flex h-[122px] w-[248px] flex-col items-center justify-center">
-                <h1 className="text-[40px] font-bold text-blue-800">+6.633</h1>
+                <h1 className="text-[40px] font-bold text-blue-500 dark:text-blue-800">
+                  +6.633
+                </h1>
                 <p className="text-[16px]">Logísticas reversas</p>
               </div>
             </CarouselItem>
 
             <CarouselItem className="pt-10 md:basis-1/2 lg:basis-1/4">
               <div className="flex h-[122px] w-[248px] flex-col items-center justify-center">
-                <h1 className="text-[40px] font-bold text-blue-800">+1.977</h1>
+                <h1 className="text-[40px] font-bold text-blue-500 dark:text-blue-800">
+                  +1.977
+                </h1>
                 <p className="text-[16px]">Reposições por roubo</p>
               </div>
             </CarouselItem>
 
             <CarouselItem className="pt-10 md:basis-1/2 lg:basis-1/4">
               <div className="flex h-[122px] w-[248px] flex-col items-center justify-center">
-                <h1 className="text-[40px] font-bold text-blue-800">+28.070</h1>
+                <h1 className="text-[40px] font-bold text-blue-500 dark:text-blue-800">
+                  +28.070
+                </h1>
                 <p className="text-[16px]">Pedidos enviados</p>
               </div>
             </CarouselItem>
 
             <CarouselItem className="pt-10 md:basis-1/2 lg:basis-1/4">
               <div className="flex h-[122px] w-[248px] flex-col items-center justify-center">
-                <h1 className="text-[40px] font-bold text-blue-800">+9.463</h1>
+                <h1 className="text-[40px] font-bold text-blue-500 dark:text-blue-800">
+                  +9.463
+                </h1>
                 <p className="text-[16px]">Telas substituídas</p>
               </div>
             </CarouselItem>
@@ -89,12 +104,12 @@ export default function AboutUs() {
       </div>
 
       <div className="flex flex-col items-center py-10 text-center dark:text-zinc-300">
-        <div className="flex flex-col pb-10 font-bold md:text-4xl">
+        <div className="flex flex-col pb-10 text-2xl font-bold md:text-4xl">
           <div>Toop</div>
           <div>Inovação e Agilidade</div>
         </div>
 
-        <div className="max-w-[50%] pb-16">
+        <div className="max-w-[80%] pb-16 text-sm md:max-w-[50%] md:text-xl">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
           itaque consequuntur perspiciatis et ut veniam facilis nemo libero
           voluptatibus tempora, animi iusto nesciunt reprehenderit obcaecati
@@ -121,12 +136,12 @@ export default function AboutUs() {
       </div>
 
       <div className="flex flex-col items-center gap-8 px-[10%] py-20">
-        <div className="hidden h-[1px] w-full bg-gray-500 md:block" />
+        <div className="h-[1px] w-full bg-gray-500" />
       </div>
 
-      <div className="flex flex-col gap-[100px] px-[20%]">
+      <div className="hidden flex-col gap-[100px] px-[20%] md:flex">
         <div className="relative flex flex-row">
-          <div className="absolute inset-0 left-[-20px] top-[80px] -z-10 h-[200px] w-[250px] rounded-xl bg-blue-700" />
+          <div className="absolute inset-0 left-[-20px] top-[80px] -z-10 h-[200px] w-[250px] rounded-xl bg-blue-500 dark:bg-blue-800" />
 
           <Image
             src={recepcao}
@@ -145,13 +160,16 @@ export default function AboutUs() {
         <div className="relative flex flex-row justify-end pl-[20%]">
           <div className="flex flex-col justify-center pr-10 text-sm font-bold dark:text-zinc-300">
             <h1 className="pb-4 text-right text-2xl">
-              Somos a <span className="text-blue-700">ATM Outsourcing</span>
+              Somos a{' '}
+              <span className="text-blue-500 dark:text-blue-800">
+                ATM Outsourcing
+              </span>
             </h1>
             <p className="text-right text-xl">a solução mais completa</p>
             <p className="text-right text-xl">em telefonia móvel corporativa</p>
           </div>
 
-          <div className="absolute inset-0 left-[75%] top-[35%] -z-10 h-[72%] w-[27%] rounded-xl bg-blue-700" />
+          <div className="absolute inset-0 left-[75%] top-[35%] -z-10 h-[72%] w-[27%] rounded-xl bg-blue-500 dark:bg-blue-800" />
 
           <Image
             src={recepcao}
@@ -161,14 +179,57 @@ export default function AboutUs() {
         </div>
       </div>
 
+      <div className="flex flex-col md:hidden">
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src={recepcao}
+            alt="imagem atm"
+            className="w-[350px] rounded-3xl"
+          />
+          <div className="absolute inset-0 left-[2%] top-[240%] -z-10 h-[200px] w-[250px] rounded-xl bg-blue-500 dark:bg-blue-800" />
+
+          <div className="flex px-10 pt-10 text-center text-sm font-bold dark:text-zinc-300">
+            A ATM Outsourcing foi fundada em 2007, atualmente, somos líderes no
+            segmento de Outsourcing de mobilidade no Brasil. Fabricante de
+            Smartphones corporativos homologados pela ANATEL e desenvolvedora de
+            sistemas de MDM, a ATM Outsourcing é referência em soluções de
+            mobilidade corporativa.
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col pt-20 md:hidden">
+        <div className="flex flex-col items-center justify-center">
+          <Image
+            src={recepcao}
+            alt="imagem atm"
+            className="w-[350px] rounded-3xl"
+          />
+          <div className="absolute inset-0 left-[38%] top-[310%] -z-10 h-[200px] w-[250px] rounded-xl bg-blue-500 dark:bg-blue-800" />
+
+          <div className="flex flex-col justify-center text-sm font-bold dark:text-zinc-300">
+            <h1 className="pb-2 pt-10 text-center text-2xl">
+              Somos a{' '}
+              <span className="text-blue-500 dark:text-blue-800">
+                ATM Outsourcing
+              </span>
+            </h1>
+            <p className="text-center text-xl">a solução mais completa</p>
+            <p className="text-center text-xl">
+              em telefonia móvel corporativa
+            </p>
+          </div>
+        </div>
+      </div>
+
       <div className="flex flex-col items-center gap-8 px-[10%] py-20">
         <div className="hidden h-[1px] w-full bg-gray-500 md:block" />
       </div>
 
-      <div className="flex flex-row justify-center gap-10 pb-20 md:min-h-[700px]">
-        <div className="flex max-w-[400px] flex-col items-center rounded-xl border p-10 shadow-[0_0_15px_5px_rgba(59,130,246,0.5)]">
+      <div className="flex flex-col items-center justify-center gap-10 pb-20 md:min-h-[700px] md:flex-row">
+        <div className="flex flex-col items-center rounded-full border p-10 shadow-[0_0_15px_5px_rgba(59,130,246,0.5)] md:h-[600px] md:max-w-[400px] md:rounded-xl">
           <svg
-            className="h-[200px] w-[200px] text-blue-800"
+            className="h-[100px] w-[100px] text-blue-500 dark:text-blue-800 md:h-[200px] md:w-[200px]"
             stroke="currentColor"
             fill="currentColor"
             strokeWidth="0"
@@ -177,14 +238,17 @@ export default function AboutUs() {
           >
             <path d="M275.3 250.5c7 7.4 18.4 7.4 25.5 0l108.9-114.2c31.6-33.2 29.8-88.2-5.6-118.8-30.8-26.7-76.7-21.9-104.9 7.7L288 36.9l-11.1-11.6C248.7-4.4 202.8-9.2 172 17.5c-35.3 30.6-37.2 85.6-5.6 118.8l108.9 114.2zm290 77.6c-11.8-10.7-30.2-10-42.6 0L430.3 402c-11.3 9.1-25.4 14-40 14H272c-8.8 0-16-7.2-16-16s7.2-16 16-16h78.3c15.9 0 30.7-10.9 33.3-26.6 3.3-20-12.1-37.4-31.6-37.4H192c-27 0-53.1 9.3-74.1 26.3L71.4 384H16c-8.8 0-16 7.2-16 16v96c0 8.8 7.2 16 16 16h356.8c14.5 0 28.6-4.9 40-14L564 377c15.2-12.1 16.4-35.3 1.3-48.9z"></path>
           </svg>
-          <p className="pb-10 text-4xl font-bold text-blue-700">Propósito</p>
-          <p className="text-center">
+          <p className="pb-2 text-2xl font-bold text-blue-500 dark:text-blue-800 md:pb-10 md:text-4xl">
+            Propósito
+          </p>
+          <p className="w-[200px] text-center md:w-auto">
             “Ajudar e inspirar com exemplo para dar sentido a vida em Deus.”
           </p>
         </div>
-        <div className="flex max-w-[400px] flex-col items-center rounded-xl border p-10 shadow-[0_0_15px_5px_rgba(59,130,246,0.5)]">
+
+        <div className="flex flex-col items-center rounded-full border p-10 shadow-[0_0_15px_5px_rgba(59,130,246,0.5)] md:h-[600px] md:max-w-[400px] md:rounded-xl">
           <svg
-            className="h-[200px] w-[200px] text-blue-800"
+            className="h-[100px] w-[100px] text-blue-500 dark:text-blue-800 md:h-[200px] md:w-[200px]"
             stroke="currentColor"
             fill="none"
             strokeWidth="2"
@@ -199,8 +263,10 @@ export default function AboutUs() {
             <path d="M15 6v3h3l3 -3h-3v-3z"></path>
             <path d="M15 9l-3 3"></path>
           </svg>
-          <p className="pb-10 text-4xl font-bold text-blue-700">Missão</p>
-          <p className="text-center">
+          <p className="pb-2 text-2xl font-bold text-blue-500 dark:text-blue-800 md:pb-10 md:text-4xl">
+            Missão
+          </p>
+          <p className="w-[300px] pb-10 text-center md:w-auto">
             “A nossa missão é ajudar os clientes e colaboradores a garantirem
             uma qualidade de vida e realização no seu trabalho com suporte da
             tecnologia.”
@@ -208,9 +274,9 @@ export default function AboutUs() {
         </div>
       </div>
 
-      <div className="flex h-screen flex-row items-center justify-center gap-10">
-        <Image src={valores} alt="Valores" className="w-[40%]" />
-        <Image src={grupoAtm} alt="Valores" className="w-[40%]" />
+      <div className="flex flex-col items-center justify-center gap-10 pb-20 md:h-screen md:flex-row">
+        <Image src={valores} alt="Valores" className="w-[80%] md:w-[40%]" />
+        <Image src={grupoAtm} alt="Valores" className="w-[80%] md:w-[40%]" />
       </div>
     </div>
   )
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -30,95 +30,87 @@ export function Footer() {
 
         <div className="gap-24 md:grid md:grid-cols-4">
           <div className="flex flex-col gap-2 pb-4 md:items-center">
-            <div>
-              <span className="text-base font-bold underline underline-offset-4 dark:text-zinc-400">
-                Links úteis
-              </span>
-            </div>
+            <span className="text-center text-base font-bold underline underline-offset-4 dark:text-zinc-400 md:text-left">
+              Links úteis
+            </span>
 
-            <div className="md:space-y-2">
-              <span className="flex cursor-pointer items-center gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
+            <div className="md:items-left flex flex-col items-center md:space-y-2">
+              <span className="flex cursor-pointer gap-2 font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
                 Suporte
               </span>
 
-              <span className="flex cursor-pointer items-center gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
+              <span className="flex cursor-pointer gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
                 Planos
               </span>
 
-              <span className="flex cursor-pointer items-center gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
+              <span className="flex cursor-pointer gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
                 Contatos
               </span>
 
-              <span className="flex cursor-pointer items-center gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
+              <span className="flex cursor-pointer gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
                 Sobre nós
               </span>
 
-              <span className="flex cursor-pointer items-center gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
+              <span className="flex cursor-pointer gap-2 text-sm font-bold text-zinc-600 hover:text-zinc-800 hover:underline dark:text-zinc-300">
                 Blog
               </span>
             </div>
           </div>
 
-          <div className="font-roboto flex flex-col pb-4 text-xl font-bold tracking-tighter">
-            <div className="flex flex-col gap-2 md:items-center">
-              <div>
-                <span className="text-base font-bold underline underline-offset-4 dark:text-zinc-400">
-                  Escritório São Paulo - SP
-                </span>
-              </div>
+          <div className="font-roboto flex flex-col items-center gap-2 pb-4 text-xl font-bold tracking-tighter md:items-start md:items-center">
+            <div>
+              <span className="text-base font-bold underline underline-offset-4 dark:text-zinc-400">
+                Escritório São Paulo - SP
+              </span>
+            </div>
 
-              <div className="space-y-4">
-                <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300 md:text-justify">
-                  <MapPin className="h-5 w-5 flex-shrink-0" />
-                  Rua Arandu, 566 - Brooklin São Paulo - SP, 04562-031
-                </span>
-              </div>
+            <div className="space-y-4">
+              <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300 md:text-justify">
+                <MapPin className="h-5 w-5 flex-shrink-0" />
+                Rua Arandu, 566 - Brooklin São Paulo - SP, 04562-031
+              </span>
             </div>
           </div>
 
-          <div className="font-roboto flex flex-col pb-4 text-lg font-bold tracking-tighter">
-            <div className="flex flex-col gap-2 md:items-center">
-              <div>
-                <span className="text-base underline underline-offset-4 dark:text-zinc-400">
-                  Matriz
-                </span>
-              </div>
+          <div className="font-roboto flex flex-col items-center gap-2 pb-4 text-lg font-bold tracking-tighter md:items-start md:items-center">
+            <div>
+              <span className="text-base underline underline-offset-4 dark:text-zinc-400">
+                Matriz
+              </span>
+            </div>
 
-              <div className="space-y-1 md:space-y-2">
-                <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300 md:text-justify">
-                  <MapPin className="h-5 w-5 flex-shrink-0" />
-                  Georgina Business Park - Av Anísio Haddad, 8001, Milan Sul,
-                  Sala 413, São José do Rio Preto - SP, 15091-751
-                </span>
+            <div className="space-y-1 md:space-y-2">
+              <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300 md:text-justify">
+                <MapPin className="h-5 w-5 flex-shrink-0" />
+                Georgina Business Park - Av Anísio Haddad, 8001, Milan Sul, Sala
+                413, São José do Rio Preto - SP, 15091-751
+              </span>
 
-                <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300">
-                  <Phone className="h-5 w-5" />
-                  (17) 3211-5353 <br />
-                  0800 777 4343
-                </span>
+              <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300">
+                <Phone className="h-5 w-5" />
+                (17) 3211-5353 <br />
+                0800 777 4343
+              </span>
 
-                <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300">
-                  <Mail className="h-5 w-5" />
-                  contato@atmoutsourcing.com.br
-                </span>
-              </div>
+              <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300">
+                <Mail className="h-5 w-5" />
+                contato@atmoutsourcing.com.br
+              </span>
             </div>
           </div>
 
-          <div className="flex flex-col text-xl font-bold tracking-tighter">
-            <div className="flex flex-col gap-2 md:items-center">
-              <div>
-                <span className="text-base font-bold underline underline-offset-4 dark:text-zinc-400">
-                  Horários comercial
-                </span>
-              </div>
+          <div className="flex flex-col items-center gap-2 text-xl font-bold tracking-tighter md:items-start">
+            <div>
+              <span className="text-base font-bold underline underline-offset-4 dark:text-zinc-400">
+                Horários comercial
+              </span>
+            </div>
 
-              <div className="space-y-2">
-                <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300 md:text-justify">
-                  <CalendarDays className="h-4 w-4 flex-shrink-0" />
-                  Seg. a Sex. das 8h às 19h
-                </span>
-              </div>
+            <div className="space-y-2">
+              <span className="flex items-center gap-2 text-sm text-zinc-600 dark:text-zinc-300 md:text-justify">
+                <CalendarDays className="h-4 w-4 flex-shrink-0" />
+                Seg. a Sex. das 8h às 19h
+              </span>
             </div>
           </div>
         </div>
