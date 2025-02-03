faf3b6ff3c9a2c8f7fee10867845cb845cafd854
Lucas <Lucas.amadio@atmsistema.com.br>
2025-02-03 10:34:18 -0300

    ajuste texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,11 +73,11 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col gap-6 pb-10">
-          <h1 className="text-center font-plusJakartaSans text-xl font-bold text-zinc-900 dark:text-zinc-100">
+          <h1 className="text-center font-plusJakartaSans text-xl font-bold text-zinc-900 dark:text-zinc-300">
             TOOP – Tecnologia Inteligente para Gestão Empresarial
           </h1>
 
-          <div className="flex flex-col gap-3 px-0 text-center text-sm font-normal md:text-justify">
+          <div className="flex flex-col gap-3 px-0 text-center text-sm font-normal dark:text-zinc-300 md:text-justify">
             <span>
               O TOOP, desenvolvido pela ATM Outsourcing, é a solução definitiva
               para otimizar e transformar a gestão do seu negócio. Com
@@ -85,13 +85,13 @@ export default function Home() {
               e proporciona insights estratégicos para uma tomada de decisão
               mais ágil e assertiva.
             </span>
-            <span>
+            <span className="dark:text-zinc-300">
               Seja para otimizar fluxos operacionais, integrar equipes ou
               potencializar resultados, o TOOP combina automação e IA para. O
               TOOP automatiza processos, oferecer eficiência e inovação na
               gestão empresarial.
             </span>
-            <span>
+            <span className="dark:text-zinc-300">
               Gestão inteligente, automatizada e conectada ao futuro. Fale
               conosco e descubra como o TOOP pode revolucionar a sua empresa💡
             </span>
